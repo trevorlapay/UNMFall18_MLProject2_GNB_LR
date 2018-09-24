9ff1f03277803bbd8fa0386a271755068eb01611
La Pay <tlapay@s1001993.local>
2018-09-24 11:45:12 -0600

    working NB classifier

--- ConstructionZone.py
+++ ConstructionZone.py
@@ -8,8 +8,7 @@ Created on Fri Sep 21 2018
 import argparse
 import pandas as pd
 import numpy as np
-import math, operator, functools
-import scipy.stats as stats
+import math
 
 
 # MAP constants
@@ -17,23 +16,28 @@ classColumn = 61189
 vocabularyLength = 61188
 beta = 1/vocabularyLength
 alpha = 1 + beta
-allClasses = pd.read_csv("newsgrouplabels.txt", header=None)
-
+labelsFile = open('newsgrouplabels.txt', "r")
+allLabels = labelsFile.read().splitlines()
 classWordCounts = dict([(1, 154382), (2, 138499), (3, 116141), (4, 103535), (5, 90456),
                         (6, 144656), (7, 64094), (8, 107399), (9, 110928), (10, 124537), (11, 143087),
                         (12, 191242), (13, 97924), (14, 158930), (15, 162521), (16, 236747), (17, 172257),
                         (18, 280067), (19, 172670), (20, 135764)])
+priors = [.04025, .052, .051833333333333335, .05358333333333333, .050166666666666665, .0525, .0515, .051166666666666666,
+          .05408333333333333, .052333333333333336, .05383333333333333, .05325, .05216666666666667, .05175,
+          .05308333333333334, .05425, .04833333333333333, .049416666666666664, .03891666666666667, .035583333333333335]
+
 # Generate a dataframe for priors.
+# this should never change, so only do it once and store the values in an array.
 def generateMLEpriors(trainingDf):
     # totalClasses = len(allClasses.index)
     numDocs = len(trainingDf)
     priors = []
-    for num, val in allClasses.iterrows():
-        prior = len(trainingDf.loc[trainingDf[classColumn] == (num+1)])/numDocs
+    for num in range(20):
+        prior = len(trainingDf.loc[trainingDf['labelId'] == (num+1)])/numDocs
         priors.append(prior)
-    return pd.DataFrame(priors)
+    return priors
 
-# Approach: read in priors and map probability files.
+# Approach: read from priors list and map probability files.
 # take the probability MAP matrix and apply log2 to all elements
 # first, since we need to use those for log likelihoods.
 # To classify a given row in the testing data, iterate through
@@ -44,38 +48,43 @@ def generateMLEpriors(trainingDf):
 #   Sum all values in this row, and add to the log 2 of the prior for that class.
 #   Finally, add the sums to a list and pick the argmax out of that list, which
 #   should be the likeliest class for the element in the data.
-def naiveBayesClassify(trainingDf):
+def naiveBayesClassify(testingDf):
     # take argmax of the probability of a test example belonging to a given class across all features.
-    priors = pd.read_csv("priors.csv", header=None).drop(0, axis=1).drop(0, axis=0)
+    # drop doc_id column.
+    testingDf = testingDf.drop('docId', axis=1)
     probabilityMatrix = pd.read_csv("map_probabilities.csv", header=None).drop(0, axis=1).drop(0, axis=0)
     # apply log2 first for calculating log sums
     probabilityMatrix = probabilityMatrix.applymap(math.log2)
     calculated_probabilities = []
+    classList = []
     # classes = rows in priors vector.
     # wondering how to do this with matrix multiplication. Too slow iteratively.
-    for row in trainingDf:
-        for num, val in priors.iterrows():
-            sumProb = trainingDf.iloc[row].multiply(probabilityMatrix.iloc[num - 1]).values.sum()
-            calculated_probabilities.append(sumProb + math.log2(val.iloc[0]))
-        print(np.argmax(calculated_probabilities))
+    for index, row in testingDf.iterrows():
+        for num, val in enumerate(priors):
+            sumProb = row.multiply(probabilityMatrix.iloc[num]).values.sum()
+            calculated_probabilities.append(sumProb + math.log2(val))
+        classList.append((np.argmax(calculated_probabilities) + 1))
         calculated_probabilities = []
-
+    labelCol = pd.Series(classList)
+    return labelCol
 
 
 # Generate a MAP dataframe for probabilites across classes and features.
+# This needs to be updated every time the beta value changes.
 def generateMAPmatrix(trainingDf):
-    vocabDf = pd.read_csv("vocabulary.txt", header=None)
+    vocabFile = open('vocabulary.txt', "r")
+    allWords = vocabFile.read().splitlines()
     listoflists = []
-    for numClass, newsGroup in allClasses.iterrows():
+    for numClass, newsGroup in enumerate(allLabels):
         rowdata = []
         totalWordsInClass = classWordCounts.get(numClass + 1)
-        trainingDfByClass = trainingDf.loc[trainingDf[61189] == (numClass + 1)]
-        for numWord, word  in vocabDf.iterrows():
+        trainingDfByClass = trainingDf.loc[trainingDf['labelId'] == (numClass + 1)]
+        for numWord, word  in enumerate(allWords):
             if len(trainingDfByClass) > 1:
                 # Counting the words in a class is too slow using dataframes.
                 # There is probably a faster way to do this (either that, or create a
                 # file that just stores these values in a vector or something)
-                countWordsinClass = trainingDfByClass[numWord].sum()
+                countWordsinClass = trainingDfByClass[numWord+1].sum()
             else:
                 # don't skip zero counts since we are using MAP hallucinated values.
                 countWordsinClass = totalWordsInClass = 0
@@ -83,8 +92,15 @@ def generateMAPmatrix(trainingDf):
             denominator = totalWordsInClass + ((alpha - 1)*vocabularyLength)
             rowdata.append(numerator/denominator)
         listoflists.append(rowdata)
-        print(rowdata)
     return pd.DataFrame(listoflists)
+
+def generateSubmissionFileNB(testingDataFile="testing.csv", answersDataFile="answers.csv"):
+    vocabFile = open('vocabulary.txt', "r")
+    allWords = vocabFile.read().splitlines()
+    colNames = ['docId'] + list(range(1, len(allWords) + 1))
+    testingDF = pd.read_csv(testingDataFile, header=None, names=colNames)
+    testingDF['classification'] = naiveBayesClassify(testingDF)
+    testingDF.to_csv(answersDataFile)
 
 
 def main():
@@ -92,14 +108,16 @@ def main():
     # vocabFile = open('vocabulary.txt', "r")
     # allWords = vocabFile.read().splitlines()
     # colNames = ['docId'] + list(range(1, len(allWords) + 1)) + ['labelId']
-    trainingDf = pd.read_csv("training.csv", header=None, nrows =300).to_sparse(fill_value=0)\
-        .drop(0, axis=1).drop(61189, axis=1)
-    # priorsdf = generateMLEpriors(trainingdf)
+    # vocabFile = open('vocabulary.txt', "r")
+    # allWords = vocabFile.read().splitlines()
+    # colNames = ['docId'] + list(range(1, len(allWords) + 1))
+    # trainingDf = pd.read_csv("training.csv", header=None,
+    #                        names=colNames).to_sparse(fill_value=0)
+    # priorsdf = generateMLEpriors(trainingDf)
     # priorsdf.to_csv("priors.csv")
-    # mapdf = generateMAPmatrix(trainingdf)
+    # mapdf = generateMAPmatrix(trainingDf)
     # mapdf.to_csv("map_probabilities.csv")
-    # I'm running this against a tiny subset of the training to see if the classifier is working.
-    # Currently, it is not. 
-    naiveBayesClassify(trainingDf)
+    # The classifier scores 100% against the training data (I didn't do any Beta-tuning yet)
+    generateSubmissionFileNB()
 
 if __name__ == "__main__": main()
