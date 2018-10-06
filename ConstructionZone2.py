58fc053b388f8f4fca27a20f3a52ec8529a0b293
Luke Hanks <hankyusa@gmail.com>
2018-10-06 11:56:19 -0600

    Split training set into label based subsets. Switch to SciPy sparse matrices. Add data saving and loading functionality.
    SciPy sparse matrices are definitly more time and space efficient than Pandas sparse dataframes. I still needed Pandas to read the original CSV and split it into label based subsets, but after that the SciPy sparse matrices are all we need.
    
    Loading the data from a npz file takes less than a second. If you run ConstructionZone2.py, then it will try loading the data from ConstructionZone2Vars.npz. If it fails to load the variables, then it will try to load the raw data from the training.csv (takes a long time), dirive the desired set of variables (takes some time), and save the variables to ConstructionZone2Vars.npz (takes no time).
    
    Sorry. This commit has too many changes. I should have broken it up.

--- ConstructionZone2.py
+++ ConstructionZone2.py
@@ -1,34 +1,62 @@
-import argparse as ap
 import pandas as pd
 import scipy as sp
 import numpy as np
 
-#pd.read_csv("training.csv", header=None, nrows=200).to_csv("sparse_training_tiny.csv",header=False)
-
-labelsFile = open('newsgrouplabels.txt', "r")
-allLabels = labelsFile.read().splitlines()
-labelsFile.close()
-numLabels = len(allLabels)
-allLabelIds = list(range(1,numLabels+1))
-
-vocabFile = open('vocabulary.txt', "r")
-allWords = vocabFile.read().splitlines()
-vocabFile.close()
-numWords = len(allWords)
-allWordIds = list(range(1,numWords+1))
-
-colNames = ['docId']+list(range(1,len(allWords)+1))+['labelId']
-trainingDf = pd.read_csv("training.csv",header=None,
-                         names=colNames).to_sparse(fill_value=0)
-print(trainingDf)
-
-labelProportions = trainingDf['labelId'].value_counts(normalize=True)
-print(labelProportions)
-labelProportions.plot(kind='pie')
-
-labelWordSums = trainingDf.groupby('labelId')[allWordIds].agg(np.sum).to_sparse(fill_value=0)
-print(labelWordSums)
-
-labelSums = labelWordSums.sum(axis=1)
-print(labelSums)
-labelSums.plot(kind='bar')
+try:
+    # True  -> start from scratch
+    # False -> load from file
+    if False: raise Exception()
+    npzFile = np.load('ConstructionZone2Vars.npz')
+    
+    allLabels = list(npzFile['allLabels'])
+    numLabels = npzFile['numLabels'].item()
+    allLabelIds = list(npzFile['allLabelIds'])
+    allWords = list(npzFile['allWords'])
+    numWords = npzFile['numWords'].item()
+    allWordIds = list(npzFile['allWordIds'])
+    listOfLabelMatixes = list(npzFile['listOfLabelMatixes'])
+    labelCounts = list(npzFile['labelCounts'])
+    labelProportions = list(npzFile['labelProportions'])
+    labelsWordSums = list(npzFile['labelsWordSums'])
+    labelSums = list(npzFile['labelSums'])
+except:
+    labelsFile = open('newsgrouplabels.txt', 'r')
+    allLabels = labelsFile.read().splitlines()
+    labelsFile.close()
+    numLabels = len(allLabels)
+    allLabelIds = list(range(1,numLabels+1))
+    
+    vocabFile = open('vocabulary.txt', 'r')
+    allWords = vocabFile.read().splitlines()
+    vocabFile.close()
+    numWords = len(allWords)
+    allWordIds = list(range(1,numWords+1))
+    
+    colNames = ['docId']+list(range(1,len(allWords)+1))+['labelId']
+    trainingDf = pd.read_csv('training.csv',header=None,dtype=np.int32,
+                             names=colNames).to_sparse(fill_value=0)
+    print(trainingDf)
+    
+    docsGroupedByLabel = trainingDf.groupby('labelId')[allWordIds]
+    
+    listOfLabelMatixes = [sp.sparse.csr_matrix(docsGroupedByLabel.get_group(labelID)) for labelID in allLabelIds]
+    
+    labelCounts = [labelMatrix.shape[0] for labelMatrix in listOfLabelMatixes]
+    tempSum = sum(labelCounts)
+    labelProportions = [labelCount/tempSum for labelCount in labelCounts]
+    
+    labelsWordSums = [ sp.transpose(labelMatrix).dot(np.ones(labelMatrix.shape[0],dtype=np.int32)) for labelMatrix in listOfLabelMatixes]
+    
+    labelSums = [sum(labelWordSums) for labelWordSums in labelsWordSums]
+    
+    np.savez('ConstructionZone2Vars.npz',allLabels = allLabels,
+                                        numLabels = numLabels,
+                                        allLabelIds = allLabelIds,
+                                        allWords = allWords,
+                                        numWords = numWords,
+                                        allWordIds = allWordIds,
+                                        listOfLabelMatixes = listOfLabelMatixes,
+                                        labelCounts = labelCounts,
+                                        labelProportions = labelProportions,
+                                        labelsWordSums = labelsWordSums,
+                                        labelSums = labelSums)
