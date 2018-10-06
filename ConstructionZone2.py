6652d008cbbc03852c59a8000e4fdda89176aef3
Luke Hanks <hankyusa@gmail.com>
2018-10-06 15:19:10 -0600

    minor exception handling and refactoring

--- ConstructionZone2.py
+++ ConstructionZone2.py
@@ -2,61 +2,70 @@ import pandas as pd
 import scipy as sp
 import numpy as np
 
+npzFileName = "ConstructionZone2Vars.npz"
+
 try:
     # True  -> start from scratch
     # False -> load from file
     if False: raise Exception()
-    npzFile = np.load('ConstructionZone2Vars.npz')
+    npzFile = np.load(npzFileName)
     
-    allLabels = list(npzFile['allLabels'])
-    numLabels = npzFile['numLabels'].item()
-    allLabelIds = list(npzFile['allLabelIds'])
-    allWords = list(npzFile['allWords'])
-    numWords = npzFile['numWords'].item()
-    allWordIds = list(npzFile['allWordIds'])
-    listOfLabelMatixes = list(npzFile['listOfLabelMatixes'])
-    labelCounts = list(npzFile['labelCounts'])
-    labelProportions = list(npzFile['labelProportions'])
-    labelsWordSums = list(npzFile['labelsWordSums'])
-    labelSums = list(npzFile['labelSums'])
+    allClasses              = list(npzFile['allClasses'])
+    allClassIDs             = list(npzFile['allClassIDs'])
+    allWords                = list(npzFile['allWords'])
+    allWordIDs              = list(npzFile['allWordIDs'])
+    classMatixes            = list(npzFile['classMatixes'])
+    classDocCounts          = list(npzFile['classDocCounts'])
+    classProportions        = list(npzFile['classProportions'])
+    classWordInstanceCounts = list(npzFile['classWordInstanceCounts'])
+    classTotalWordCounts    = list(npzFile['classTotalWordCounts'])
 except:
-    labelsFile = open('newsgrouplabels.txt', 'r')
-    allLabels = labelsFile.read().splitlines()
-    labelsFile.close()
-    numLabels = len(allLabels)
-    allLabelIds = list(range(1,numLabels+1))
+    print("Could not load variables from npz file. Attempting to read raw data.")
+    # TODO Print error details.
+    try:
+        classesFile = open('newsgrouplabels.txt', 'r')
+        allClasses = classesFile.read().splitlines()
+        classesFile.close()
+    except:
+        print("Could not read news groups from newsgrouplabels.txt.")
+        raise
+    allClassIDs = list(range(1,len(allClasses)+1))
+    try:
+        vocabFile = open('vocabulary.txt', 'r')
+        allWords = vocabFile.read().splitlines()
+        vocabFile.close()
+    except:
+        print("Could not read vocabulary from vocabulary.txt.")
+        raise
+    allWordIDs = list(range(1,len(allWords)+1))
     
-    vocabFile = open('vocabulary.txt', 'r')
-    allWords = vocabFile.read().splitlines()
-    vocabFile.close()
-    numWords = len(allWords)
-    allWordIds = list(range(1,numWords+1))
+    colNames = ['docID']+allWordIDs+['classID']
+    try:
+        trainingDf = pd.read_csv('training.csv',header=None,dtype=np.int32,
+                                 names=colNames).to_sparse(fill_value=0)
+    except:
+        print("Could not read training data from training.csv.")
+        raise
+    docsGroupedByClass = trainingDf.groupby('classID')[allWordIDs]
+    classMatixes = [sp.sparse.csr_matrix(docsGroupedByClass.get_group(classID)) for classID in allClassIDs]
     
-    colNames = ['docId']+list(range(1,len(allWords)+1))+['labelId']
-    trainingDf = pd.read_csv('training.csv',header=None,dtype=np.int32,
-                             names=colNames).to_sparse(fill_value=0)
-    print(trainingDf)
+    trainingDf = None
+    docsGroupedByClass = None
     
-    docsGroupedByLabel = trainingDf.groupby('labelId')[allWordIds]
+    classDocCounts = [classMatrix.shape[0] for classMatrix in classMatixes]
+    tempSum = sum(classDocCounts)
+    classProportions = [classCount/tempSum for classCount in classDocCounts]
     
-    listOfLabelMatixes = [sp.sparse.csr_matrix(docsGroupedByLabel.get_group(labelID)) for labelID in allLabelIds]
+    classWordInstanceCounts = [ sp.transpose(classMatrix).dot(np.ones(classMatrix.shape[0],dtype=np.int32)) for classMatrix in classMatixes]
     
-    labelCounts = [labelMatrix.shape[0] for labelMatrix in listOfLabelMatixes]
-    tempSum = sum(labelCounts)
-    labelProportions = [labelCount/tempSum for labelCount in labelCounts]
+    classTotalWordCounts = [sum(classWordSums) for classWordSums in classWordInstanceCounts]
     
-    labelsWordSums = [ sp.transpose(labelMatrix).dot(np.ones(labelMatrix.shape[0],dtype=np.int32)) for labelMatrix in listOfLabelMatixes]
-    
-    labelSums = [sum(labelWordSums) for labelWordSums in labelsWordSums]
-    
-    np.savez('ConstructionZone2Vars.npz',allLabels = allLabels,
-                                        numLabels = numLabels,
-                                        allLabelIds = allLabelIds,
-                                        allWords = allWords,
-                                        numWords = numWords,
-                                        allWordIds = allWordIds,
-                                        listOfLabelMatixes = listOfLabelMatixes,
-                                        labelCounts = labelCounts,
-                                        labelProportions = labelProportions,
-                                        labelsWordSums = labelsWordSums,
-                                        labelSums = labelSums)
+    np.savez(npzFileName,   allClasses              = allClasses,
+                            allClassIDs             = allClassIDs,
+                            allWords                = allWords,
+                            allWordIDs              = allWordIDs,
+                            classMatixes            = classMatixes,
+                            classDocCounts          = classDocCounts,
+                            classProportions        = classProportions,
+                            classWordInstanceCounts = classWordInstanceCounts,
+                            classTotalWordCounts    = classTotalWordCounts)
