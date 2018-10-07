70ecc6befc81a82feb05b25053202c172563915e
Luke Hanks <hankyusa@gmail.com>
2018-10-06 18:55:53 -0600

    Improve var names. Save vars using pickle instead. Add timers.

--- ConstructionZone2.py
+++ ConstructionZone2.py
@@ -1,71 +1,110 @@
 import pandas as pd
 import scipy as sp
 import numpy as np
+import pickle
+import time
 
 npzFileName = "ConstructionZone2Vars.npz"
+pklFileName = "ConstructionZone2Vars.pkl"
+sTime = time.time()
 
 try:
     # True  -> start from scratch
     # False -> load from file
     if False: raise Exception()
-    npzFile = np.load(npzFileName)
     
-    allClasses              = list(npzFile['allClasses'])
-    allClassIDs             = list(npzFile['allClassIDs'])
-    allWords                = list(npzFile['allWords'])
-    allWordIDs              = list(npzFile['allWordIDs'])
-    classMatixes            = list(npzFile['classMatixes'])
-    classDocCounts          = list(npzFile['classDocCounts'])
-    classProportions        = list(npzFile['classProportions'])
-    classWordInstanceCounts = list(npzFile['classWordInstanceCounts'])
-    classTotalWordCounts    = list(npzFile['classTotalWordCounts'])
-except:
-    print("Could not load variables from npz file. Attempting to read raw data.")
-    # TODO Print error details.
+    with open(pklFileName, 'rb') as pklFile:
+        (allClasses,
+            allClassIDs,
+            allWords,
+            allWordIDs,
+            classMatixes,
+            classDocCounts,
+            classProportions,
+            wordCountsInClasses,
+            totalWordsInClasses) = pickle.load(pklFile)
+        pklFile.close()
+        print("Loaded variables in "+str(time.time()-sTime))
+except Exception as err:
+    print("Could not load variables from npz file. Error details:")
+    print(type(err))
+    print(err.args)
+    print(err)
+    print("Attempting to read raw data instead.")
+    sTime = time.time()
     try:
         classesFile = open('newsgrouplabels.txt', 'r')
         allClasses = classesFile.read().splitlines()
         classesFile.close()
+        print("Read allClasses in "+str(time.time()-sTime))
     except:
         print("Could not read news groups from newsgrouplabels.txt.")
         raise
     allClassIDs = list(range(1,len(allClasses)+1))
+    sTime = time.time()
     try:
         vocabFile = open('vocabulary.txt', 'r')
         allWords = vocabFile.read().splitlines()
         vocabFile.close()
+        print("Read allWords in "+str(time.time()-sTime))
     except:
         print("Could not read vocabulary from vocabulary.txt.")
         raise
+    sTime = time.time()
     allWordIDs = list(range(1,len(allWords)+1))
+    print("Created allWordIDs in "+str(time.time()-sTime))
     
+    sTime = time.time()
     colNames = ['docID']+allWordIDs+['classID']
+    print("Created colNames in "+str(time.time()-sTime))
+    
+    sTime = time.time()
     try:
         trainingDf = pd.read_csv('training.csv',header=None,dtype=np.int32,
                                  names=colNames).to_sparse(fill_value=0)
+        print("Read trainingDf in "+str(time.time()-sTime))
     except:
         print("Could not read training data from training.csv.")
         raise
+    sTime = time.time()
     docsGroupedByClass = trainingDf.groupby('classID')[allWordIDs]
     classMatixes = [sp.sparse.csr_matrix(docsGroupedByClass.get_group(classID)) for classID in allClassIDs]
+    print("Created classMatixes in "+str(time.time()-sTime))
     
+    colNames = None
+    del colNames
     trainingDf = None
+    del trainingDf
     docsGroupedByClass = None
+    del docsGroupedByClass
     
+    sTime = time.time()
     classDocCounts = [classMatrix.shape[0] for classMatrix in classMatixes]
     tempSum = sum(classDocCounts)
     classProportions = [classCount/tempSum for classCount in classDocCounts]
+    print("Calculated classProportions in "+str(time.time()-sTime))
     
-    classWordInstanceCounts = [ sp.transpose(classMatrix).dot(np.ones(classMatrix.shape[0],dtype=np.int32)) for classMatrix in classMatixes]
+    tempSum = None
+    del tempSum
     
-    classTotalWordCounts = [sum(classWordSums) for classWordSums in classWordInstanceCounts]
+    sTime = time.time()
+    wordCountsInClasses = [ sp.transpose(classMatrix).dot(np.ones(classMatrix.shape[0],dtype=np.int32)) for classMatrix in classMatixes]
+    print("Calculated wordCountsInClasses in "+str(time.time()-sTime))
     
-    np.savez(npzFileName,   allClasses              = allClasses,
-                            allClassIDs             = allClassIDs,
-                            allWords                = allWords,
-                            allWordIDs              = allWordIDs,
-                            classMatixes            = classMatixes,
-                            classDocCounts          = classDocCounts,
-                            classProportions        = classProportions,
-                            classWordInstanceCounts = classWordInstanceCounts,
-                            classTotalWordCounts    = classTotalWordCounts)
+    sTime = time.time()
+    totalWordsInClasses = [sum(classWordCounts) for classWordCounts in wordCountsInClasses]
+    print("Calculated totalWordsInClasses in "+str(time.time()-sTime))
+
+    sTime = time.time()
+    with open(pklFileName, 'wb') as pklFile:
+        pickle.dump([allClasses,
+                        allClassIDs,
+                        allWords,
+                        allWordIDs,
+                        classMatixes,
+                        classDocCounts,
+                        classProportions,
+                        wordCountsInClasses,
+                        totalWordsInClasses], pklFile)
+        pklFile.close()
+        print("Dumped variables in "+str(time.time()-sTime))
