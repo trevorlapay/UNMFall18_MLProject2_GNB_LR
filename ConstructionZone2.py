f478cbe4f470f4c64afcbdae29a27ff1e7e94d80
Luke Hanks <hankyusa@gmail.com>
2018-10-11 19:50:44 -0600

    Put a lot of things in functions

--- ConstructionZone2.py
+++ ConstructionZone2.py
@@ -8,8 +8,7 @@ import pandas as pd
 import random
 import math
 
-
-pklFileName = "ConstructionZone2Vars.pkl"
+PKL_FILE_NAME = "ConstructionZone2Vars.pkl"
 
 #%% Define time related itmes
 sTime = time.time()
@@ -23,11 +22,11 @@ try:
     # True  -> start from scratch
     # False -> load from file
     if False: raise Exception()
-    with open(pklFileName, 'rb') as pklFile:
-        (allClasses,
-         allWords,
-         allClassExamples,
-         testingData) = pickle.load(pklFile)
+    with open(PKL_FILE_NAME, 'rb') as pklFile:
+        (ALL_CLASSES,
+         ALL_WORDS,
+         ALL_CLASS_EXAMPLES,
+         TEST_EXAMPLES) = pickle.load(pklFile)
         pklFile.close()
         reportRunTime("Loaded variables")
 except Exception as err:
@@ -36,28 +35,28 @@ except Exception as err:
     print(err.args)
     print(err)
     print("\nAttempting to read raw data instead.\nAll runtimes given in seconds.")
-    #%% Read in allClasses.
+    #%% Read in ALL_CLASSES.
     try:
         classesFile = open('newsgrouplabels.txt', 'r')
-        allClasses = classesFile.read().splitlines()
+        ALL_CLASSES = classesFile.read().splitlines()
         classesFile.close()
-        allClasses = dict(zip(range(1, len(allClasses)+1), allClasses))
-        reportRunTime("Read allClasses")
+        ALL_CLASSES = dict(zip(range(1, len(ALL_CLASSES)+1), ALL_CLASSES))
+        reportRunTime("Read ALL_CLASSES")
     except:
         print("Could not read news groups from newsgrouplabels.txt.")
         raise
-    #%% Read in allWords.
+    #%% Read in ALL_WORDS.
     try:
         vocabFile = open('vocabulary.txt', 'r')
-        allWords = vocabFile.read().splitlines()
+        ALL_WORDS = vocabFile.read().splitlines()
         vocabFile.close()
-        allWords = dict(zip(range(1, len(allWords)+1), allWords))
-        reportRunTime("Read allWords")
+        ALL_WORDS = dict(zip(range(1, len(ALL_WORDS)+1), ALL_WORDS))
+        reportRunTime("Read ALL_WORDS")
     except:
         print("Could not read vocabulary from vocabulary.txt.")
         raise
     #%% Create colNames.
-    colNames = ['docID']+list(allWords.keys())+['classID']
+    colNames = ['docID']+list(ALL_WORDS.keys())+['classID']
     reportRunTime("Created colNames")
     #%% Read trainingDF.
     try:
@@ -67,11 +66,11 @@ except Exception as err:
     except:
         print("Could not read training data from training.csv.")
         raise
-    #%% Create allClassExamples.
-    allClassExamples = {classID : (list(examples['docID']),
-                                sp.sparse.csr_matrix(examples[list(allWords.keys())]))
+    #%% Create ALL_CLASS_EXAMPLES.
+    ALL_CLASS_EXAMPLES = {classID : (list(examples['docID']),
+                                sp.sparse.csr_matrix(examples[list(ALL_WORDS.keys())]))
         for classID, examples in trainingDF.groupby('classID')}
-    reportRunTime("Created allClassExamples")
+    reportRunTime("Created ALL_CLASS_EXAMPLES")
     trainingDF = None
     del trainingDF
     docsGroupedByClass = None
@@ -84,19 +83,19 @@ except Exception as err:
     except:
         print("Could not read testing data from testing.csv.")
         raise
-    #%% Create testingData
-    testingData = (testingDF['docID'].tolist(), sp.sparse.csr_matrix(testingDF[colNames[1:-1]]))
-    reportRunTime("Created testingData")
+    #%% Create TEST_EXAMPLES
+    TEST_EXAMPLES = (testingDF['docID'].tolist(), sp.sparse.csr_matrix(testingDF[colNames[1:-1]]))
+    reportRunTime("Created TEST_EXAMPLES")
     colNames = None
     del colNames
     testingDF = None
     del testingDF
     #%% Dump variables.
-    with open(pklFileName, 'wb') as pklFile:
-        pickle.dump((allClasses,
-                     allWords,
-                     allClassExamples,
-                     testingData), pklFile)
+    with open(PKL_FILE_NAME, 'wb') as pklFile:
+        pickle.dump((ALL_CLASSES,
+                     ALL_WORDS,
+                     ALL_CLASS_EXAMPLES,
+                     TEST_EXAMPLES), pklFile)
         pklFile.close()
         reportRunTime("Dumped variables")
 #%% Define data splitting functions.
@@ -119,81 +118,82 @@ def splitClassExamples(classExamples, splitPorportion = 0.5):
     for classID, examples in classExamples.items():
         subSet1[classID], subSet2[classID] = splitExamples(examples, splitPorportion)
     return subSet1, subSet2
-metaConfusionMatrix = np.zeros((len(allClasses),len(allClasses)), dtype=np.int32)
-numValidations = 20
-for i in range(numValidations):
-    #%% Seportate allClassExamples into training and validation sets.
-    trainingClassExamples, validationClassExamples = splitClassExamples(allClassExamples, 0.75)
-#    reportRunTime("Split allClassExamples into trainingClassExamples and validationClassExamples")
-    #%% Calculate classProportions.
-    classDocCounts = [len(docIDs) for classID, (docIDs, dataMat) in trainingClassExamples.items()]
-    temp = sum(classDocCounts)
-    classProportions = [classCount/temp for classCount in classDocCounts]
-#    reportRunTime("Calculated classProportions")
-    #%% Calculate wordCountsInClasses.
-    wordCountsInClasses = [sp.transpose(dataMat).dot(np.ones(len(docIDs), dtype=np.int32))
-        for classID, (docIDs, dataMat) in trainingClassExamples.items()]
-#    reportRunTime("Calculated wordCountsInClasses")
-    #%% Calculate totalWordsInClasses.
-    totalWordsInClasses = [sum(classWordCounts) for classWordCounts in wordCountsInClasses]
-#    reportRunTime("Calculated totalWordsInClasses")
-    #%% Naive Bayes Learning
-    #for beta in np.linspace(0,1,1000):
-    beta = .01 # This is the best preforming beta value Trevor found.
-    alpha = 1 + beta
-    temp = (alpha-1)*len(allWords)
-    mapMmatrix = np.array([(wordCountsInClasses[classID-1]+(alpha-1))/(totalWordsInClasses[classID-1]+temp)
-                  for classID in allClasses.keys()])
-    mapMmatrixLog = np.vectorize(math.log2)(mapMmatrix)
-#    reportRunTime("Calculated mapMmatrix and mapMmatrixLog")
-    def naiveBayesClassify(dataMat):
-        likelyhoods = dataMat.dot(mapMmatrixLog.transpose())
-        b = np.repeat(np.array([list(allClasses.keys())]), len(likelyhoods), axis=0)
-        return b[np.arange(len(likelyhoods)), np.argmax(likelyhoods, axis=1)]
-    #%% Naive Bayes Validation
-    confusionMatrix = np.zeros((len(allClasses),len(allClasses)), dtype=np.int32)
-    for trueClassID, (docIDs, dataMat) in validationClassExamples.items():
-        predictions = naiveBayesClassify(dataMat)
+#%% Define general validation and testing functions
+def validateClassifier(classExamples, classifyFunction, **kwargs):
+    confusionMat = np.zeros((len(ALL_CLASSES),len(ALL_CLASSES)), dtype=np.int32)
+    for trueClassID, (docIDs, dataMat) in classExamples.items():
+        predictions = classifyFunction(dataMat, **kwargs)
         classConfusion = np.unique(predictions, return_counts=True)
         for predictedClassID, count in zip(list(classConfusion[0]), list(classConfusion[1])):
-            confusionMatrix[trueClassID-1][predictedClassID-1] = count
-#    reportRunTime("Calculated predictions for validationClassExamples set")
-    metaConfusionMatrix += confusionMatrix
-#%% Plot confusion matrix
-metaConfusionMatrix = (np.vectorize(round)(metaConfusionMatrix / numValidations)).astype(np.int32)
-noDiagConMat = metaConfusionMatrix.copy()
-for i in range(len(noDiagConMat)):
-    noDiagConMat[i, i] = 0
-noDiagConMat *= -1
-fig, conMatAx = plt.subplots(figsize=(10, 10))
-conMatIm = conMatAx.matshow(noDiagConMat,cmap=plt.get_cmap("Reds").reversed())
-conMatAx.set_xticks(np.arange(len(allClasses)))
-conMatAx.set_yticks(np.arange(len(allClasses)))
-conMatAx.set_xticklabels(allClasses.values())
-conMatAx.set_yticklabels(allClasses.values())
-conMatAx.set_xlabel("Predicted Classes", size=14)
-conMatAx.set_ylabel("True Classes", size=14)
-conMatAx.tick_params(top=False, bottom=True, labeltop=False, labelbottom=True)
-plt.setp(conMatAx.get_xticklabels(), rotation=60, ha="right", rotation_mode="anchor")
-textcolors=["black", "white"]
-for i in range(len(allClasses)):
-    for j in range(len(allClasses)):
-        if metaConfusionMatrix[i, j] != 0:
-            if i == j: color = textcolors[0]
-            else: color=textcolors[conMatIm.norm(noDiagConMat[i,j])<conMatIm.norm(noDiagConMat.max())/3]
-            text = conMatAx.text(j, i, metaConfusionMatrix[i, j],
-                           ha="center", va="center", size=10, color=color)
-conMatAx.set_title("Average Confusion Matrix of "+str(numValidations)+" Naive Bayes Validation Rounds",size=16)
-fig.tight_layout()
-fig.savefig(nowStr()+'metaConfusionMatrix.png')
-plt.show()
-reportRunTime("Plotted metaConfusionMatrix")
-#%% Naive Bayes Testing
-predictions = naiveBayesClassify(testingData[1])
-reportRunTime("Calculated predictions for testingData")
-#%% Naive Bayes Testing Submission File
-answersDF = pd.DataFrame()
-answersDF['id'] = pd.Series(testingData[0])
-answersDF['class'] = pd.Series(predictions)
-answersDF.to_csv(nowStr()+'answers.csv', index=False)
-reportRunTime("Wrote submission file")
+            confusionMat[trueClassID-1][predictedClassID-1] = count
+    return confusionMat
+def testClassifier(examples, classifyFunction, **kwargs):
+    predictions = classifyFunction(examples[1], **kwargs)
+    answersDF = pd.DataFrame()
+    answersDF['id'] = pd.Series(examples[0])
+    answersDF['class'] = pd.Series(predictions)
+    answersDF.to_csv(nowStr()+'answers.csv', index=False)
+#%% Define confusion matrix plotting function
+def plotConfusionMat(confMat, title="Confusion Matrix"):
+    noDiagConfMat = confMat.copy()
+    for i in range(len(noDiagConfMat)):
+        noDiagConfMat[i, i] = 0
+    noDiagConfMat *= -1
+    fig, confMatAx = plt.subplots(figsize=(10, 10))
+    confMatIm = confMatAx.matshow(noDiagConfMat, cmap=plt.get_cmap("Reds").reversed())
+    confMatAx.set_xticks(np.arange(len(ALL_CLASSES)))
+    confMatAx.set_yticks(np.arange(len(ALL_CLASSES)))
+    confMatAx.set_xticklabels(ALL_CLASSES.values())
+    confMatAx.set_yticklabels(ALL_CLASSES.values())
+    confMatAx.set_xlabel("Predicted Classes", size=14)
+    confMatAx.set_ylabel("True Classes", size=14)
+    confMatAx.tick_params(top=False, bottom=True, labeltop=False, labelbottom=True)
+    plt.setp(confMatAx.get_xticklabels(), rotation=60, ha="right", rotation_mode="anchor")
+    textcolors=["black", "white"]
+    threshold = confMatIm.norm(noDiagConfMat.max()) / 3
+    for i in range(len(ALL_CLASSES)):
+        for j in range(len(ALL_CLASSES)):
+            if confMat[i, j] != 0:
+                if i != j and confMatIm.norm(noDiagConfMat[i,j]) < threshold:
+                    color = textcolors[1]
+                else:
+                    color = textcolors[0]
+                confMatAx.text(j, i, confMat[i, j], ha="center", va="center", size=10, color=color)
+    confMatAx.set_title(title, size=16)
+    fig.tight_layout()
+    fig.savefig(nowStr()+'confusionMatrix.png')
+    plt.show()
+#%% Define Naive Bayes functions
+def naiveBayesTrain(classExamples, mapMatAsLog=True):
+#    #%% Calculate classProportions.
+#    classDocCounts = [len(docIDs) for classID, (docIDs, dataMat) in classExamples.items()]
+#    temp = sum(classDocCounts)
+#    classProportions = [classCount/temp for classCount in classDocCounts]
+    wordCountsInClasses = [sp.transpose(dataMat).dot(np.ones(len(docIDs), dtype=np.int32))
+        for classID, (docIDs, dataMat) in classExamples.items()]
+    totalWordsInClasses = [sum(classWordCounts) for classWordCounts in wordCountsInClasses]
+    #for beta in np.linspace(0,1,1000):
+    beta = .01 # This is the best preforming beta value Trevor found.
+    temp = (beta)*len(ALL_WORDS)
+    mapMat = np.array([(wordCountsInClasses[classID-1]+(beta))
+                       /(totalWordsInClasses[classID-1]+temp)
+                  for classID in ALL_CLASSES.keys()])
+    return np.vectorize(math.log2)(mapMat) if mapMatAsLog else mapMat
+def naiveBayesClassify(dataMat, mapMat):
+    likelyhoods = dataMat.dot(mapMat.transpose())
+    b = np.repeat(np.array([list(ALL_CLASSES.keys())]), len(likelyhoods), axis=0)
+    return b[np.arange(len(likelyhoods)), np.argmax(likelyhoods, axis=1)]
+#%% Train and validate Naive Bayes with different random selections of tarining and validation sets
+metaConfusionMat = np.zeros((len(ALL_CLASSES),len(ALL_CLASSES)), dtype=np.int32)
+numValidations = 20
+for i in range(numValidations):
+    trainingClassExamples, validationClassExamples = splitClassExamples(ALL_CLASS_EXAMPLES, 0.75)
+    mapMat = naiveBayesTrain(trainingClassExamples)
+    metaConfusionMat += validateClassifier(validationClassExamples, naiveBayesClassify, mapMat=mapMat)
+#%% Plot metaConfusionMat
+metaConfusionMat = (np.vectorize(round)(metaConfusionMat / numValidations)).astype(np.int32)
+plotConfusionMat(metaConfusionMat,
+                 "Average Confusion Matrix of "+str(numValidations)+" Naive Bayes Rounds")
+#%% Test Naive Bayes
+testClassifier(TEST_EXAMPLES, naiveBayesClassify, mapMat=mapMat)
+reportRunTime("Naive Bayes training, validating and testing")
